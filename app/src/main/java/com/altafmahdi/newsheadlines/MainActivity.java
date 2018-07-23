635b4a7c50d178a1cc036218a8b43aa3e88cde53
Altaf-Mahdi <altaf.mahdi@gmail.com>
2018-07-23 13:53:37 +0100

    NewsHeadlines: add a splash screen

--- app/src/main/java/com/altafmahdi/newsheadlines/MainActivity.java
+++ app/src/main/java/com/altafmahdi/newsheadlines/MainActivity.java
@@ -155,7 +155,7 @@ public class MainActivity extends AppCompatActivity {
         if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
             mDrawerLayout.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+            finishAffinity();
         }
     }
 
